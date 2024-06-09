e4b751e4c282646b0a0cb27559303cac50c6a7ef
L4wson <84114068+L4wson@users.noreply.github.com>
2024-06-09 15:14:17 -0700

    first 3

---
+++ Unit1/Session1/ProblemSet1/problem2.py
@@ -0,0 +1,6 @@
+def todays_mood():
+    mood = "Chill fr"
+    print("Today's mood: " + mood)
+
+todays_mood()
+
---
+++ Unit1/Session1/ProblemSet1/problem3.py
@@ -0,0 +1,4 @@
+# The following function accepts one parameter menu. Copy this code to your Replit and add a function call so that "Lunch today is: 🍕" is printed to the console.
+def print_menu(menu):
+    print("Lunch today is: " + menu)
+print_menu("Lunch today is: 🍕")
